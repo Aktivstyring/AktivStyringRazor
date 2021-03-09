43efad301c1d88afb06cd1b82aaffbd0bacf2e40
Thomas Fogh <thom20m1@edu.easj.dk>
2021-03-09 16:01:32 +0100

    Added Service inteface

---
+++ AktivStyringRazor/Services/Interfaces/IAktivService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AktivStyringRazor.Models;
+
+namespace AktivStyringRazor.Services.Interfaces
+{
+    public interface IAktivService
+    {
+        Task<List<Aktiv>> GetAktiverAsync();
+        Task AddAktivAsync(Aktiv aktiv);
+        Task DeleteAktivAsync(Aktiv aktiv);
+        Task<Aktiv> GetAktivById();
+
+    }
+}
