39d61446ecb29508bbdc8ce058950b2d33acb72b
CBoens <43374935+CBoens@users.noreply.github.com>
2021-03-10 15:19:28 +0100

    Create Personer.cs

---
+++ AktivStyringRazor/Models/Personer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AktivStyringRazor.Models
+{
+    public class Personer
+    {
+    }
+}
