db36482632adde706b8fd796969faa23994750af
CBoens <43374935+CBoens@users.noreply.github.com>
2021-03-15 14:55:41 +0100

    Create Aktivudleveringer.cs
    Aktivudlevering model oprettet

---
+++ AktivStyringRazor/Models/Aktivudleveringer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AktivStyringRazor.Models
+{
+    public class Aktivudleveringer
+    {
+        public int AktivID { get; set; }
+        public int PersonID { get; set; }
+        public DateTime AktivUddelt { get; set; }
+        public DateTime AktivIndsamlet { get; set; }
+        public int UdleveringsID { get; set; }
+
+        public Aktivudleveringer()
+        {
+        }
+
+        public Aktivudleveringer(int aktivId, int personId, DateTime aktivUddelt, DateTime aktivIndsamlet, int udleveringsId)
+        {
+            AktivID = aktivId;
+            PersonID = personId;
+            AktivUddelt = aktivUddelt;
+            AktivIndsamlet = aktivIndsamlet;
+            UdleveringsID = udleveringsId;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(AktivID)}: {AktivID}, {nameof(PersonID)}: {PersonID}, {nameof(AktivUddelt)}: {AktivUddelt}, {nameof(AktivIndsamlet)}: {AktivIndsamlet}, {nameof(UdleveringsID)}: {UdleveringsID}";
+        }
+    }
+}
