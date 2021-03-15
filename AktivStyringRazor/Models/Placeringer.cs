0bce746d4973fe9f92404b630d86ea6d6797c6e4
CBoens <43374935+CBoens@users.noreply.github.com>
2021-03-15 20:28:39 +0100

    Create Placeringer.cs
    Placering model oprettet

---
+++ AktivStyringRazor/Models/Placeringer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AktivStyringRazor.Models
+{
+    public class Placeringer
+    {
+        public int PlaceringID { get; set; }
+        public string Placering { get; set; }
+        public int PlaceringSort { get; set; }
+
+
+        public Placeringer()
+        {
+        }
+
+
+        public Placeringer(int placeringId, string placering, int placeringSort)
+        {
+            PlaceringID = placeringId;
+            Placering = placering;
+            PlaceringSort = placeringSort;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(PlaceringID)}: {PlaceringID}, {nameof(Placering)}: {Placering}, {nameof(PlaceringSort)}: {PlaceringSort}";
+        }
+    }
+}
