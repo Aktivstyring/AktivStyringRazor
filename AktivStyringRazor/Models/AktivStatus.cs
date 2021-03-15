4281e874c0aaf6455f7e34286862c4d6ea15aa43
CBoens <43374935+CBoens@users.noreply.github.com>
2021-03-15 13:55:00 +0100

    Create AktivStatus.cs
    AktivStatus Model oprettet

---
+++ AktivStyringRazor/Models/AktivStatus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AktivStyringRazor.Models
+{
+    public class AktivStatus
+    {
+        public int AktivStatusID { get; set; }
+        public string StatusAktiv { get; set; }
+        public int AktivStatusGrad { get; set; }
+        public int AktivStatusOrder { get; set; }
+
+        public AktivStatus()
+        {
+        }
+
+        public AktivStatus(int aktivStatusID, string statusAktiv, int aktivStatusGrad, int aktivStatusOrder)
+        {
+            AktivStatusID = aktivStatusID;
+            StatusAktiv = statusAktiv;
+            AktivStatusGrad = aktivStatusGrad;
+            AktivStatusOrder = aktivStatusOrder;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(AktivStatusID)}: {AktivStatusID}, {nameof(StatusAktiv)}: {StatusAktiv}, {nameof(AktivStatusGrad)}: {AktivStatusGrad}, {nameof(AktivStatusOrder)}: {AktivStatusOrder}";
+        }
+    }
+}
