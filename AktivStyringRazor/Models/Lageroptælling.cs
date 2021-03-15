3aa6333f7201fe540677ba7a42cb592d8e23f33e
CBoens <43374935+CBoens@users.noreply.github.com>
2021-03-15 16:44:25 +0100

    Create Lageroptælling.cs
    Lageroptælling model oprettet

---
+++ AktivStyringRazor/Models/Lageroptælling.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AktivStyringRazor.Models
+{
+    public class Lageroptælling
+    {
+        public int AktivID { get; set; }
+        public int Placering { get; set; }
+        public DateTime PlaceringDato { get; set; }
+        public string PlaceringNote { get; set; }
+        public int LinkID { get; set; }
+
+        public Lageroptælling()
+        {
+        }
+
+        public Lageroptælling(int aktivId, int placering, DateTime placeringDato, string placeringNote, int linkId)
+        {
+            AktivID = aktivId;
+            Placering = placering;
+            PlaceringDato = placeringDato;
+            PlaceringNote = placeringNote;
+            LinkID = linkId;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(AktivID)}: {AktivID}, {nameof(Placering)}: {Placering}, {nameof(PlaceringDato)}: {PlaceringDato}, {nameof(PlaceringNote)}: {PlaceringNote}, {nameof(LinkID)}: {LinkID}";
+        }
+    }
+}
