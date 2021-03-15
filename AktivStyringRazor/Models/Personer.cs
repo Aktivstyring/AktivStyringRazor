1c1e3ae7825d55d5b22c17bd9c00584d99ee9238
CBoens <43374935+CBoens@users.noreply.github.com>
2021-03-15 13:34:28 +0100

    Update Personer.cs

--- AktivStyringRazor/Models/Personer.cs
+++ AktivStyringRazor/Models/Personer.cs
@@ -7,5 +7,29 @@ namespace AktivStyringRazor.Models
 {
     public class Personer
     {
+        public string Navn { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+        public string Adresse { get; set; }
+        public string MedlemsNr { get; set; }
+
+        public Personer()
+        {
+        }
+
+        public Personer(string navn, string email, string telefon, string adresse, string medlemsNr)
+        {
+            Navn = navn;
+            Email = email;
+            Telefon = telefon;
+            Adresse = adresse;
+            MedlemsNr = medlemsNr;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{nameof(Navn)}: {Navn}, {nameof(Email)}: {Email}, {nameof(Telefon)}: {Telefon}, {nameof(Adresse)}: {Adresse}, {nameof(MedlemsNr)}: {MedlemsNr}";
+        }
     }
 }
