95c39936cd8e1ee667c2016a2e5fdb35c2bd09f3
CBoens <43374935+CBoens@users.noreply.github.com>
2021-03-15 14:18:06 +0100

    Create AktivTyper.cs
    oprettelse af Aktivtype model

---
+++ AktivStyringRazor/Models/AktivTyper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AktivStyringRazor.Models
+{
+    public class AktivTyper
+    {
+        public string AktivType { get; set; }
+        public int AktivTypeOrder { get; set; }
+        public int AktivTypeID { get; set; }
+
+        public AktivTyper()
+        {
+        }
+
+        public AktivTyper(string aktivType, int aktivTypeOrder, int aktivTypeId)
+        {
+            AktivType = aktivType;
+            AktivTypeOrder = aktivTypeOrder;
+            AktivTypeID = aktivTypeId;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{nameof(AktivType)}: {AktivType}, {nameof(AktivTypeOrder)}: {AktivTypeOrder}, {nameof(AktivTypeID)}: {AktivTypeID}";
+        }
+    }
+}
