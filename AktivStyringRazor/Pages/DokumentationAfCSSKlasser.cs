6a90f8487e0cd28c4f0090751e47c897bb5675b5
CBoens <43374935+CBoens@users.noreply.github.com>
2021-04-26 16:47:48 +0200

    css start

---
+++ AktivStyringRazor/Pages/DokumentationAfCSSKlasser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AktivStyringRazor.Pages
+{
+
+    /*
+
+
+        SearchBar
+        SeaBar
+
+        ListTitelNavn
+        LisTNvn
+
+
+        KolonneCSS:
+
+        TableHeaderKolonneListNavn
+        THKolLisNvn
+
+        TableBodyKolListItem
+        TBKolLisItem
+    
+        ListTableHeader
+        LisTHeader
+
+        TableHeaderTableRowList
+        THeaderTRLis
+    
+        ListTableBodyRow
+        LisTBodyRow
+
+        TableBodyTableRowListItem
+        TBodyTRowLisItem
+    
+
+
+
+
+    
+    
+    
+    
+    
+    
+    /*
+    public class DokumentationAfCSSKlasser
+    {
+    }
+    */
+}
